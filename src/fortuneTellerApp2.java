13a2f7187438fc4fbcdc5c14b2e1b1656eaa8acf
Deborah Akers <akersds18@gmail.com>
2019-01-05 16:54:21 -0500

    updated

--- src/fortuneTellerApp2.java
+++ src/fortuneTellerApp2.java
@@ -1,9 +1,39 @@
+import java.util.Scanner;
 
 public class fortuneTellerApp2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+	Scanner input = new Scanner(System.in);
 
+	System.out.println("What is your first name?");
+	String firstName = input.nextLine();
+	
+	System.out.println("What is your last name?");
+	String lastName = input.nextLine();
+	
+	System.out.println("Welcome " + firstName + " to Fortune Teller!");
+	System.out.println();
+	
+	System.out.println("What is your age?");
+	int numberAge = input.nextInt();
+	
+	System.out.println("What month were you born?");
+	int numberMonth = input.nextInt();
+	
+	System.out.println("What is your favorite ROYGBIV color? If you don't recognize these colors enter help");
+	String favoriteROYGBIV = input.next();
+	
+	if(favoriteROYGBIV.equalsIgnoreCase("help")) 
+	{
+		System.out.println("ROYGBIV means red, orange, yellow,green, blue, indigo, violet");
+		System.out.println("What is your favorite ROYGBIV color?");
+		favoriteROYGBIV=input.next();
 	}
+	
 
+	
+	System.out.println("How many siblings do you have?");
+	int numberSiblings = input.nextInt();
+}
 }
